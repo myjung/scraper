c4fa75a6cc4e4151256e934ddc5bc23384735d4e
rey-desktop <hexagramxix@gmaill.com>
2021-02-11 23:11:13 +0900

    페이지 순회 작동중

--- find_jobs/rocketpunch.py
+++ find_jobs/rocketpunch.py
@@ -1,54 +1,104 @@
-from scrapy import Spider
-from scrapy.item import Item, Field
-from scrapy.http import Request
 import scrapy
+from scrapy import Spider
+from scrapy.crawler import Crawler, CrawlerProcess, CrawlerRunner
+from scrapy.http import Request, Response
+from scrapy.item import Field, Item
+from scrapy.loader import ItemLoader
+from scrapy.selector import Selector
 
 from utils import get_ua
 
 
-class RocketpunchPage(Item):
-    page_number = Field()
-    companys
+class RocketpuchPage(Item):
+    """
+    페이지 별 회사 리스트 템플릿을 반환하는 아이템
+    """
 
-
-class RocketpunchJob(Item):
-    page_address = Field()
-    job_id = Field()
-    company_id = Field()
-    title = Field()
-    works = Field()
-    speicalities = Field()
-    industry = Field()
-    location = Field()
-    experienced = Field()
-    # set(계약, 정규, 인턴) 고용형태
-    # smallint 연봉 start
-    # smallint 연봉 end
-    # date 공고 수정
-    # date 공고 마감
+    page = Field()
+    company_list = Field()
 
 
 class RocketpunchPageSpider(Spider):
+    """
+    first step : https://www.rocketpunch.com/jobs
+        response headers를 통해 앞으로 사용할 requests headers를 설정함
+    second step : https://www.rocketpunch.com/api/jobs/template?page=&q=
+        첫번째 아이템 및 기저조건으로 최종 페이지를 확인 후 한꺼번에 제너레이터를 생성한다.
+    second step : https://www.rocketpunch.com/api/jobs/template?page={page_number}&q=
+        page_number에 해당하는 이터레이터를 생성하여 전체 페이지에 대해 각각의 아이템을 반환한다. (동시접속 따라 다름)
+    """
+
     name = "rocketpunch_jobs"
     hello_url = "https://www.rocketpunch.com/jobs"
     custom_settings = {
-        "USER_AGENT" : get_ua(0),
-        "DEFAULT_REQUEST_HEADERS":{
-        "authority": "www.rocketpunch.com",
-        "dnt": "1",
-        "accept": "*/*",
-        "accept-language": "ko-KR,ko;q=0.9",
-        }
+        "DOWNLOAD_DELAY": 2,
+        "USER_AGENT": get_ua(0),  # get first user_agent string
+        "DEFAULT_REQUEST_HEADERS": {
+            "dnt": "1",  # do not track me
+            "accept": "*/*",  # accept all types
+            "accept-language": "ko-KR,ko;q=0.9",
+        },
+        "SCHEDULER_DISK_QUEUE": "scrapy.squeues.PickleFifoDiskQueue",
+        "SCHEDULER_MEMORY_QUEUE": "scrapy.squeues.FifoMemoryQueue",
     }
 
-    def start_requests():
+    def start_requests(self):
         """
         Let's say hi to Rocket Punch! If this action succeeds, we can get csrf tokens.
         """
-        return [Request(url=hello_url, callback=self.hello_parser)]
+        yield Request(url=self.hello_url, callback=self.hello_parser)
 
-    def hello_parser():
+    def parse(self, response):
         pass
+
+    def hello_parser(self, response):
+        # print(response.body)
+        yield Request(
+            url="https://www.rocketpunch.com/api/jobs/template?page=&q=",
+            callback=self.first_page_parser,
+            meta={"page_number": 1}
+        )
+
+    def first_page_parser(self, response):
+        data = Selector(text=response.json()["data"]["template"], base_url=response.url)
+        end_page_number = int(
+            data.css("div.ui.pagination div.disabled.item + a::text").get()
+        )
+        self.logger.info(end_page_number)
+        yield self.page_parser(response)
+        for page_number in range(2, 4):
+            yield Request(
+                url=f"https://www.rocketpunch.com/api/jobs/template?page={page_number}&q=",
+                headers={"Referer": "https://www.rocketpunch.com/jobs"},
+                callback=self.page_parser,
+                meta={"page_number": page_number},
+            )
+
+    def page_parser(self, response):
+        """
+        현재 페이지
+        #company-list > div.company             ** 회사의 리스트
+        get attr data-company-id                회사 고유 id
+            div.content                         -----------------------------
+                >div.company-name               -----------------------------
+                    >a attr[href]               회사 상세 정보 페이지 주소
+                    h4.name::text               회사 이름
+                div.description :: text         회사 상세 정보
+                div.meta :: text                회사 직무 분야
+                div.company-jobs-detail         ** 목록의 리스트
+                    a.job-title.link attr[href] 잡 상세 정보 페이지 주소
+                    a.job-title.link :: text    잡 이름
+                    span.job-stat-info :: text  연봉 및 경력 등
+                    >div.job-dates
+                        span ~3                 마감일, [기타 근무사항], 수정일
+
+        """
+        self.logger.debug("=" * 50)
+        self.logger.debug(response.request.headers.to_unicode_dict())
+        self.logger.debug(response.meta)
+        self.logger.debug("=" * 50)
+        text = response.json()["data"]["template"]
+        selector = Selector(text=text)
 
     """
     메인 페이지에서 최종 페이지를 구한 후 전체 페이지를 순회한다.
@@ -109,3 +159,13 @@ class RocketpunchDetailSpider(Spider):
     """
 
     pass
+
+
+def main():
+    my_pro = CrawlerProcess()
+    my_pro.crawl(RocketpunchPageSpider)
+    my_pro.start()
+
+
+if __name__ == "__main__":
+    main()
